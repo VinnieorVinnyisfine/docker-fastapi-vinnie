26ce55232a9bb3767a1a2a2559e07481cab8281e
VinnieorVinnyisfine <phanguye@arcada.fi>
2026-04-18 17:28:18 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,6 +5,27 @@ from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
+rooms = [
+    {
+        "room_number": 101,
+        "type": "Single",
+        "price": 80,
+        "bookable": True
+    },
+    {
+        "room_number": 102,
+        "type": "Double",
+        "price": 120,
+        "bookable": True
+    },
+    {
+        "room_number": 201,
+        "type": "Suite",
+        "price": 200,
+        "bookable": False
+    }
+]
+
 @app.get("/")
 def read_root():
     return {"msg": "Hello Vinny", "v": "0.2"}
@@ -22,3 +43,7 @@ def get_ip(request: Request):
 def get_ip_html(request: Request):
     ip = request.client.host if request.client else "unknown"
     return f"<h1>Your public IP is {ip}</h1>"
+
+@app.get("/rooms")
+def get_rooms():
+    return rooms
